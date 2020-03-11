65ba85f12ff5dbfbc685efda6d132b2f00d8c00b
unknown <tom.miller@rice.edu>
2020-03-10 22:18:57 -0500

    Made progress on data formatting. Ready for some visual analysis.

--- Kemp_data_cleaning.R
+++ Kemp_data_cleaning.R
@@ -1,18 +1,52 @@
-## Purpose: import and 'clean' the baiting data from Cassidy Kempf (fieldwork conducted in Sept/Oct 2016)
+## Purpose: import,  'clean', and visualize the baiting data from Cassidy Kempf (fieldwork conducted in Sept/Oct 2016)
 
 ## load packages
 library(tidyverse)
-library(xlsx)
 
 ## read in data from the original file, located in MillerLab dropbox (may need to update this directory on different computers)
-dat_raw<-read.xlsx("C:\\Users\\tm9\\Dropbox\\Lab Documents\\Documents\\Ants\\Big Thicket Ants\\Cassidy Kempf\\Data\\Kempf_BTNP_ants_combined.xlsx",
-                   sheetName = "baiting data")
+bait<-read.csv("kempf_baiting_data.csv")
+str(bait)
 
-## Data cleanup:
-## replace NAs with zero.
-## replace the weird 10* observations with just NA (I think this is the best we can do) 
-dat_raw <- dat_raw %>% 
-  mutate(H.Pheidole.dentata. = ifelse(H.Pheidole.dentata.=="10*",10,H.Pheidole.dentata.)) %>% 
-  replace(is.na(.), 0)
+## there is a weird entry of 10* for one species. replace this with just 10
+bait$H.Pheidole.dentata[which(bait$H.Pheidole.dentata=="10*")] <- "10"
+## convert factor to integer
+bait$H.Pheidole.dentata <- (as.integer(levels(bait$H.Pheidole.dentata))[bait$H.Pheidole.dentata])
 
-## Now the harder part: split the honey and 
+## There is one species that has no tuna. I am going to create it, which will make the next steps easier
+bait$T.Unidentified.2 <- NA
+## another column is mis-spelled
+bait$H.Unidentified.1 <- bait$H.Undientified.1
+
+##convert wide to long
+honey <- bait[,c("Day","Site","Station","Round",
+        "H.Pheidole.dentata","H.Solenopsis.invicta",
+        "H.Tapinoma.sessile","H.Nylanderia.fulva","H.Pheidole.moerens",
+        "H.Camponotus.pennsylvanicus","H.Crematogaster.sp","H.Unidentified.1",
+        "H.Aphaenogaster.treatae","H.Unidentified.2","H.Cyphomyrmex.rimosus")]
+honey$bait<-"honey"
+names(honey)[5:15] <- str_sub(names(honey)[5:15],3)
+
+tuna <- bait[,c("Day","Site","Station","Round",
+                "T.Pheidole.dentata","T.Solenopsis.invicta",
+                 "T.Tapinoma.sessile","T.Nylanderia.fulva","T.Pheidole.moerens",
+                 "T.Camponotus.pennsylvanicus","T.Crematogaster.sp","T.Unidentified.1",
+                 "T.Aphaenogaster.treatae","T.Unidentified.2","T.Cyphomyrmex.rimosus")]
+tuna$bait<-"tuna"
+names(tuna)[5:15] <- str_sub(names(tuna)[5:15],3)
+
+##these should be the same dim
+dim(honey);dim(tuna)
+
+## read in and merge site data
+sites <- read.csv("kempf_site_data.csv") %>% 
+  mutate(site=interaction(Day,Site))
+
+## now bind, replace NA with zero, make site unique, and convert "round" to minutes
+bait_long <- left_join(bind_rows(honey,tuna)%>% 
+  replace(is.na(.), 0) %>% 
+  mutate(site=interaction(Day,Site),
+         minute=Round*20),sites,by="site")
+
+
+
+
